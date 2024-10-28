30e5fe06f426257ff087ed695405a3ad5453047c
positiveHelicopter <>
2024-10-28 22:15:41 +0800

    Added background drawable

--- app/src/main/kotlin/com/positiveHelicopter/doobyplus/screens/main/DoobScreen.kt
+++ app/src/main/kotlin/com/positiveHelicopter/doobyplus/screens/main/DoobScreen.kt
@@ -5,15 +5,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.positiveHelicopter.doobyplus.R
 
 @Composable
 internal fun DoobScreen(
-    modifier: Modifier = Modifier.Companion
+    modifier: Modifier = Modifier
 ) {
-    Box(modifier.fillMaxSize().background(colorResource(R.color.purple_200))) {
+    val background = painterResource(R.drawable.doob_background)
+    Box(modifier.fillMaxSize()
+        .background(colorResource(id = R.color.backgroundColor))
+        .paint(painter = background, contentScale = ContentScale.FillWidth)
+    ) {
 
     }
 }

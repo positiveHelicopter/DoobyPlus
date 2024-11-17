8100d5c611a8ff277a4bcd470af8f88d918c14a2
positiveHelicopter <>
2024-11-17 12:07:40 +0800

    Fixed welcome screen components overflowing on small screen

--- app/src/main/kotlin/com/positiveHelicopter/doobyplus/screens/welcome/WelcomeScreen.kt
+++ app/src/main/kotlin/com/positiveHelicopter/doobyplus/screens/welcome/WelcomeScreen.kt
@@ -3,7 +3,6 @@ package com.positiveHelicopter.doobyplus.screens.welcome
 import android.graphics.drawable.Animatable
 import android.widget.ImageView
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
@@ -16,6 +15,10 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
@@ -41,10 +44,12 @@ internal fun WelcomeScreen(
     val botInset = with(LocalDensity.current) {
         WindowInsets.Companion.systemBars.getBottom(LocalDensity.current).toDp()
     }
-    Box(modifier = modifier.fillMaxSize()
-        .background(colorResource(R.color.color_welcome_green))) {
+    Column(modifier = modifier.fillMaxSize()
+        .background(colorResource(R.color.color_welcome_green))
+        .padding(top = 50.dp + topInset, bottom = botInset)) {
         Text(
-            modifier = modifier.fillMaxWidth().padding(top = 50.dp + topInset),
+            modifier = modifier.fillMaxWidth()
+                .weight(2f),
             text = stringResource(R.string.welcome_title),
             fontSize = 35.sp,
             color = colorResource(R.color.color_black_faded),
@@ -52,7 +57,8 @@ internal fun WelcomeScreen(
             textAlign = TextAlign.Center
         )
         AndroidView(
-            modifier = modifier.align(Alignment.Center),
+            modifier = modifier.align(Alignment.CenterHorizontally)
+                .weight(3f),
             factory = {
                 val view = ImageView(it)
                 view.setBackgroundResource(R.drawable.welcome)
@@ -64,37 +70,44 @@ internal fun WelcomeScreen(
             },
             update = {}
         )
-        Column(modifier = modifier.align(Alignment.BottomCenter)
-            .fillMaxWidth()
-            .padding(bottom = 30.dp + botInset),
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = stringResource(R.string.welcome_message),
-                modifier = modifier.fillMaxWidth().padding(20.dp),
-                textAlign = TextAlign.Center,
-                fontSize = 16.sp
+        Text(text = stringResource(R.string.welcome_message),
+            modifier = modifier.fillMaxWidth().padding(horizontal = 20.dp)
+                .weight(1.5f),
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp
+        )
+        var paddingValues by remember { mutableStateOf(PaddingValues(
+            horizontal = 50.dp, vertical = 15.dp)) }
+        Button(
+            modifier = modifier.padding(10.dp)
+                .align(Alignment.CenterHorizontally)
+                .weight(1f, fill = false),
+            onClick = {
+                askNotificationPermission { requestPermission ->
+                    requestPermission()
+                }
+            },
+            contentPadding = paddingValues,
+            colors = ButtonDefaults.buttonColors().copy(
+                containerColor = colorResource(R.color.color_dark_green)
             )
-            Button(
-                modifier = modifier.padding(10.dp),
-                onClick = {
-                    askNotificationPermission { requestPermission ->
-                        requestPermission()
+        ) {
+            Text(text = stringResource(R.string.welcome_allow_notifications),
+                fontSize = 16.sp,
+                onTextLayout = { result ->
+                    if (result.didOverflowHeight) {
+                        paddingValues = ButtonDefaults.ContentPadding
                     }
-                },
-                contentPadding = PaddingValues(horizontal = 50.dp, vertical = 15.dp),
-                colors = ButtonDefaults.buttonColors().copy(
-                    containerColor = colorResource(R.color.color_dark_green)
-                )
-            ) {
-                Text(text = stringResource(R.string.welcome_allow_notifications),
-                    fontSize = 16.sp)
-            }
-            TextButton(onClick = {
-                setIsFirstTimeNotification(false)
-            }) {
-                Text(text = stringResource(R.string.welcome_skip),
-                    fontSize = 16.sp,
-                    color = colorResource(R.color.color_dark_green))
-            }
+                })
+        }
+        TextButton(
+            modifier = modifier.align(Alignment.CenterHorizontally)
+                .weight(1f, fill = false),
+            onClick = { setIsFirstTimeNotification(false) }
+        ) {
+            Text(text = stringResource(R.string.welcome_skip),
+                fontSize = 16.sp,
+                color = colorResource(R.color.color_dark_green))
         }
     }
 }

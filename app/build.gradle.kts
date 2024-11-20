c909f3212583137dd558d324e73ec3d1510ca0c9
positiveHelicopter <positiveHelicopter>
2024-11-20 19:18:41 +0800

    Changed version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.positiveHelicopter.doobyplus"
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
